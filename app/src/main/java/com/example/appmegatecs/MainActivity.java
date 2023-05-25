628af2ea802f8b7868b84d1856295150e839a7e1
cciscoNet <101028501+ccisco077@users.noreply.github.com>
2023-05-25 14:03:54 -0500

    imports drawables, configuration menu

--- app/src/main/java/com/example/appmegatecs/MainActivity.java
+++ app/src/main/java/com/example/appmegatecs/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.appmegatecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Agrega este código para iniciar la nueva Activity
+        Intent navigation = new Intent(this, NavigationHomeActivity.class);
+        startActivity(navigation);
     }
 }
